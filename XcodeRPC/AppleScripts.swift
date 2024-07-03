0784863dd60ecd313094ec6ee113e1cb0add322f
atomtables <76572470+atomtables@users.noreply.github.com>
2024-07-03 11:49:01 -0400

    check if xcode is a running process before running any xcode applescript

--- XcodeRPC/AppleScripts.swift
+++ XcodeRPC/AppleScripts.swift
@@ -5,50 +5,68 @@
 //  Created by Adithiya Venkatakrishnan on 30/06/2024.
 //
 
+let quitXcodeScript = """
+ignoring application responses
+    tell application "Xcode" to quit
+end ignoring
+"""
+
 let getTargetScript = """
-tell application "Xcode"
-    tell active workspace document
-        get name of (get first target of (get first project))
-    end tell
+tell application "System Events"
+    set isRunning to (name of processes) contains "Xcode"
 end tell
+
+if isRunning
+    tell application "Xcode"
+        tell active workspace document
+            get name of (get first target of (get first project))
+        end tell
+    end tell
+end if
 """
 
 let getWorkspaceScript = """
-tell application "Xcode"
-    tell active workspace document
-        path
-    end tell
+tell application "System Events"
+    set isRunning to (name of processes) contains "Xcode"
 end tell
+
+if isRunning
+    tell application "Xcode"
+        tell active workspace document
+            path
+        end tell
+    end tell
+end if
 """
 
 let getFileScript = """
-tell application "Xcode-beta"
-    set last_word to (word -1 of (get name of window 1))
-    if (last_word = "Edited") then
-        name of document 1 whose name ends with (word -2 of (get name of window 1))
-    else
-        name of document 1 whose name ends with (word -1 of (get name of window 1))
-    end if
+tell application "System Events"
+    set isRunning to (name of processes) contains "Xcode"
 end tell
+
+if isRunning
+    tell application "Xcode"
+        set last_word to (word -1 of (get name of window 1))
+        if (last_word = "Edited") then
+            name of document 1 whose name ends with (word -2 of (get name of window 1))
+        else
+            name of document 1 whose name ends with (word -1 of (get name of window 1))
+        end if
+    end tell
+end if
 """
 
+/// Only returns the first document, since more than
+/// one document seems to make it mad. If one source
+/// document is open, we are probably good.
 let getSourcesScript = """
-tell application "Xcode-beta"
-    set openDocs to {}
-    repeat with doc in source documents
-        try
-            set docName to name of doc
-            if docName is not missing value then
-                copy docName to end of openDocs
-            end if
-        on error
-            -- Handle errors, e.g., when a document has no name
-        end try
-    end repeat
-    try
-        return first item of openDocs
-    on error
-
-    end try
+tell application "System Events"
+    set isRunning to (name of processes) contains "Xcode"
 end tell
+
+if isRunning
+    tell application "Xcode"
+        return name of first source document
+    end tell
+end if
 """
