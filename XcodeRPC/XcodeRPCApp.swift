d2dcde3d109c4653649ef8be06734d27af1115fc
atomtables <76572470+atomtables@users.noreply.github.com>
2024-07-03 11:46:33 -0400

    experimenting with welcome-window

--- XcodeRPC/XcodeRPCApp.swift
+++ XcodeRPC/XcodeRPCApp.swift
@@ -41,6 +41,10 @@ final class Properties: ObservableObject {
 @main
 struct XcodeRPCApp: App {
     @StateObject var info = Properties.shared
+    @Environment(\.dismissWindow) var dismiss
+
+    @State var firstLaunch = false
+    @State var count = 1
 
     init() {
         DispatchQueue.main.async {
@@ -82,9 +86,91 @@ struct XcodeRPCApp: App {
     }
 
     var body: some Scene {
+        WindowGroup(id: "firstLaunchWindow") {
+            if firstLaunch {
+                WelcomeScreen(count: $count)
+                    .toolbar {
+                        Group {
+                            Spacer()
+                            WelcomeTabBar(count: $count)
+                            Spacer()
+                        }
+                    }
+                    .onReceive(
+                        NotificationCenter.default.publisher(
+                            for: NSApplication.didBecomeActiveNotification
+                        ),
+                        perform: { _ in
+                            NSApp.mainWindow?.standardWindowButton(.zoomButton)?.isHidden = true
+                            NSApp.mainWindow?.standardWindowButton(.closeButton)?.isHidden = true
+                            NSApp.mainWindow?.standardWindowButton(.miniaturizeButton)?.isHidden = true
+                        }
+                    )
+            } else {
+                HStack {}
+                    .onAppear {
+                        dismiss(id: "firstLaunchWindow")
+                    }
+            }
+        }
+        .windowStyle(HiddenTitleBarWindowStyle())
+
         MenuBarExtra("XcodeRPC", systemImage: info.image) {
             ContentView()
                 .environmentObject(info)
+        }
+    }
+}
+
+struct WelcomeTabBar: View {
+    @Binding var count: Int
+    var body: some View {
+        HStack {
+            Image(systemName: count == 1 ? "circlebadge.fill" : "circlebadge")
+                .symbolRenderingMode(.hierarchical)
+                .foregroundStyle(Color.accentColor)
+            Image(systemName: count == 2 ? "circlebadge.fill" : "circlebadge")
+                .symbolRenderingMode(.hierarchical)
+                .foregroundStyle(Color.accentColor)
+            Image(systemName: count == 3 ? "circlebadge.fill" : "circlebadge")
+                .symbolRenderingMode(.hierarchical)
+                .foregroundStyle(Color.accentColor)
+            Image(systemName: count == 4 ? "circlebadge.fill" : "circlebadge")
+                .symbolRenderingMode(.hierarchical)
+                .foregroundStyle(Color.accentColor)
+        }
+        .offset(x: -20)
+    }
+}
+
+struct WelcomeScreen: View {
+    @Binding var count: Int
+
+    var body: some View {
+        HStack {
+            Image(systemName: "chevron.compact.left")
+            Spacer()
+            if count == 1 {
+                HStack {
+                    Image(nsImage: NSImage(named: "XcodeRPC")!)
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                    VStack(alignment: .leading) {
+                        Text("XcodeRPC")
+                            .font(.title)
+                            .bold()
+                        Text("by atomtables")
+                    }
+                }
+            } else if count == 2 {
+                Text("Tab 2").font(.title).foregroundColor(.blue)
+            } else if count == 3 {
+                Text("Tab 3").font(.title).foregroundColor(.green)
+            } else if count == 4 {
+                Text("Tab 4").font(.title).foregroundColor(.red)
+            }
+            Spacer()
+            Image(systemName: "chevron.compact.right")
         }
     }
 }
