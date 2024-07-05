b1da108dd21c1fa5b9a0a355cda0ebd415a36e94
atomtables <76572470+atomtables@users.noreply.github.com>
2024-07-04 22:40:29 -0400

    welcome screen development

--- XcodeRPC/XcodeRPCApp.swift
+++ XcodeRPC/XcodeRPCApp.swift
@@ -44,9 +44,11 @@ final class Properties: ObservableObject {
 struct XcodeRPCApp: App {
     @StateObject var info = Properties.shared
     @Environment(\.dismissWindow) var dismiss
+    @Environment(\.openWindow) var openWindow
 
-    @State var firstLaunch = false
+    @State var firstLaunch = true
     @State var count = 1
+    @State var disableNext = false
 
     init() {
         DispatchQueue.main.async {
@@ -90,7 +92,7 @@ struct XcodeRPCApp: App {
     var body: some Scene {
         WindowGroup(id: "firstLaunchWindow") {
             if firstLaunch {
-                WelcomeScreen(count: $count)
+                WelcomeScreen(count: $count, firstLaunch: $firstLaunch, disableNext: $disableNext)
                     .toolbar {
                         Group {
                             Spacer()
@@ -109,9 +111,9 @@ struct XcodeRPCApp: App {
                         }
                     )
                     .task {
-                        for window in NSApplication.shared.windows {
-                            window.level = .floating
-                        }
+//                        for window in NSApplication.shared.windows {
+//                            window.level = .floating
+//                        }
                     }
                     .environmentObject(info)
             } else {
@@ -124,8 +126,12 @@ struct XcodeRPCApp: App {
         .windowStyle(HiddenTitleBarWindowStyle())
 
         MenuBarExtra("XcodeRPC", systemImage: info.image) {
-            ContentView()
-                .environmentObject(info)
+            if firstLaunch {
+                Text("Please continue setup.")
+            } else {
+                ContentView()
+                    .environmentObject(info)
+            }
         }
     }
 }
@@ -147,13 +153,15 @@ struct WelcomeTabBar: View {
                 .symbolRenderingMode(.hierarchical)
                 .foregroundStyle(Color.accentColor)
         }
-        .offset(x: -20)
+        .offset(x: -35)
     }
 }
 
 struct WelcomeScreen: View {
     @EnvironmentObject var info: Properties
     @Binding var count: Int
+    @Binding var firstLaunch: Bool
+    @Binding var disableNext: Bool
 
     @State private var leftHover: Bool = false
     @State private var rightHover: Bool = false
@@ -188,7 +196,12 @@ struct WelcomeScreen: View {
 
                 Spacer()
 
-                WelcomeTabView(count: $count, geometry: geometry)
+                WelcomeTabView(
+                    count: $count,
+                    firstLaunch: $firstLaunch,
+                    geometry: geometry,
+                    disableNext: $disableNext
+                )
 
                 Spacer()
 
@@ -196,18 +209,18 @@ struct WelcomeScreen: View {
                 Image(systemName: "chevron.compact.right")
                     .scaleEffect(2)
                     .padding()
-                    .foregroundStyle(count == 4 ? .quinary : .tertiary)
+                    .foregroundStyle(count == 4 || disableNext ? .quinary : .tertiary)
                     .background(
                         rightHover ? Color.gray.opacity(0.25) : Color.clear
                     )
                     .cornerRadius(10)
                     .onHover { active in
                         withAnimation {
-                            rightHover = active && count != 4
+                            rightHover = active && count != 4 && !disableNext
                         }
                     }
                     .onTapGesture {
-                        if count != 4 {
+                        if count != 4 && !disableNext {
                             withAnimation {
                                 count += 1
                             }
@@ -223,6 +236,8 @@ struct WelcomeScreen: View {
 
 struct WelcomeTabView: View {
     @Binding var count: Int
+    @Binding var firstLaunch: Bool
+    @Binding var disableNext: Bool
     @State var normalCount: Int
     let geometry: GeometryProxy
 
@@ -231,9 +246,14 @@ struct WelcomeTabView: View {
     @State var thirdTabTransition: Edge = .trailing
     @State var fourthTabTransition: Edge = .leading
 
-    init(count: Binding<Int>, geometry: GeometryProxy) {
+    @State var xcodePermissionsReceived = false
+    @State var eventsPermissionsReceived = false
+
+    init(count: Binding<Int>, firstLaunch: Binding<Bool>, geometry: GeometryProxy, disableNext: Binding<Bool>) {
         self._count = count
         self._normalCount = State(initialValue: 1)
+        self._firstLaunch = firstLaunch
+        self._disableNext = disableNext
         self.geometry = geometry
 
         self.normalCount = self.count
@@ -257,16 +277,18 @@ struct WelcomeTabView: View {
                     HStack {
                         VStack {
                             Image(systemName: "person.3.fill")
-                                .scaleEffect(2)
-                                .padding()
+                                .font(.largeTitle)
+                                .padding([.top, .horizontal])
+                                .padding(.bottom, 10)
                             Text("Save and display your activity in Xcode to all of your friends on Discord!")
                                 .frame(width: 180)
                                 .multilineTextAlignment(.center)
                         }
                         VStack {
                             Image(systemName: "photo.on.rectangle.angled")
-                                .scaleEffect(2)
-                                .padding()
+                                .font(.largeTitle)
+                                .padding([.top, .horizontal])
+                                .padding(.bottom, 10)
                             Text("Show off your icons, current open file, and workspace using Rich Presence!")
                                 .frame(width: 180)
                                 .multilineTextAlignment(.center)
@@ -275,13 +297,33 @@ struct WelcomeTabView: View {
                 }
                 .transition(.move(edge: firstTabTransition))
             } else if normalCount == 2 {
-                Text("Tab 2").font(.title).foregroundColor(.blue)
-                    .transition(.move(edge: secondTabTransition))
+                WelcomeRequestPermissionsView(
+                    disableNext: $disableNext,
+                    count: $count
+                )
+                .frame(width: geometry.size.width - 240)
+                .transition(.move(edge: secondTabTransition))
             } else if normalCount == 3 {
-                Text("Tab 3").font(.title).foregroundColor(.green)
+                Text("To Be Implemented: Configure how you want your RPC to show up")
                     .transition(.move(edge: thirdTabTransition))
             } else if normalCount == 4 {
-                Text("Tab 4").font(.title).foregroundColor(.red)
+                VStack {
+                    HStack {
+                        Image(nsImage: NSImage(named: "XcodeRPC")!)
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                        VStack(alignment: .leading) {
+                            Text("XcodeRPC")
+                                .font(.title)
+                                .bold()
+                            Text("by atomtables")
+                        }
+                    }
+                    Text("If you like this project, give it a star on GitHub!")
+                    Button("Begin...") {
+                        firstLaunch = false
+                    }
+                }
                     .transition(.move(edge: fourthTabTransition))
             }
         }
@@ -292,17 +334,19 @@ struct WelcomeTabView: View {
             if old - new == 1 {
                 switch old {
                 case 2:
+                    self.disableNext = false
                     firstTabTransition = .leading
                     secondTabTransition = .trailing
                 case 3:
                     secondTabTransition = .leading
                     thirdTabTransition = .trailing
                 case 4:
+                    self.disableNext = false
                     thirdTabTransition = .leading
                     fourthTabTransition = .trailing
                 default: break
                 }
-            } 
+            }
             /// We are going forwards
             else {
                 switch old {
@@ -310,9 +354,11 @@ struct WelcomeTabView: View {
                     firstTabTransition = .leading
                     secondTabTransition = .trailing
                 case 2:
+                    self.disableNext = false
                     secondTabTransition = .leading
                     thirdTabTransition = .trailing
                 case 3:
+                    self.disableNext = false
                     thirdTabTransition = .leading
                     fourthTabTransition = .trailing
                 default: break
@@ -320,6 +366,144 @@ struct WelcomeTabView: View {
             }
             withAnimation {
                 normalCount = new
+            }
+        }
+    }
+}
+
+struct WelcomeRequestPermissionsView: View {
+    @State var xcodePermissionsReceived: Bool = false
+    @State var eventsPermissionsReceived: Bool = false
+    @Binding var disableNext: Bool
+    @Binding var count: Int
+
+    var body: some View {
+        return VStack {
+            Text("Permissions").font(.largeTitle)
+                .onChange(of: xcodePermissionsReceived) { _, new in
+                    disableNext = !(new && eventsPermissionsReceived)
+                }
+                .onChange(of: eventsPermissionsReceived) { _, new in
+                    disableNext = !(new && xcodePermissionsReceived)
+                }
+                .onChange(of: count) { _, _ in
+                    print("its changing :skull:")
+                    disableNext = !(xcodePermissionsReceived && eventsPermissionsReceived)
+                }
+                .onAppear {
+                    disableNext = !(xcodePermissionsReceived && eventsPermissionsReceived)
+                }
+            Text("We need your permission to access your Xcode activity.")
+                .font(.headline)
+            Divider()
+            HStack {
+                Image(systemName: "hammer.circle.fill")
+                    .font(.largeTitle)
+                    .padding([.top, .horizontal])
+                    .padding(.bottom, 10)
+                VStack(alignment: .leading) {
+                    Text("Xcode")
+                        .bold()
+                        .font(.title2)
+                    Text(
+                        "Grant permission for XcodeRPC to " +
+                        "use AppleEvents to see your activity in Xcode. " +
+                        "Xcode may open during this process."
+                    )
+                    .frame(width: 300, height: nil, alignment: .leading)
+                    .multilineTextAlignment(.leading)
+                }
+                Spacer()
+                Button {
+                    let permission = runAppleScript(
+                        script: testXcodeScript
+                    )
+                    if permission != nil {
+                        xcodePermissionsReceived = true
+                    }
+                } label: {
+                    Text(xcodePermissionsReceived ? "Permission Granted" : "Request Permission")
+                        .font(.headline)
+                        .foregroundColor(xcodePermissionsReceived ? .gray : .white)
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .cornerRadius(15)
+                        .shadow(radius: 5)
+                }
+                .disabled(xcodePermissionsReceived)
+            }
+            Divider()
+            HStack {
+                Image(systemName: "eye.circle.fill")
+                    .font(.largeTitle)
+                    .padding([.top, .horizontal])
+                    .padding(.bottom, 10)
+                VStack(alignment: .leading) {
+                    Text("System Events")
+                        .bold()
+                        .font(.title2)
+                    Text("Grant permission for XcodeRPC to access " +
+                         "System Events to see when Xcode is running. " +
+                         "This will be used to only make sure Xcode is active."
+                    )
+                        .frame(width: 300, height: nil, alignment: .leading)
+                        .multilineTextAlignment(.leading)
+                }
+                Spacer()
+                Button {
+                    let permission = runAppleScript(
+                        script: testEventsScript
+                    )
+                    if permission != nil {
+                        eventsPermissionsReceived = true
+                    }
+                } label: {
+                    Text(eventsPermissionsReceived ? "Permission Granted" : "Request Permission")
+                        .font(.headline)
+                        .foregroundColor(eventsPermissionsReceived ? .gray : .white)
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .cornerRadius(15)
+                        .shadow(radius: 5)
+                }
+                .disabled(eventsPermissionsReceived)
+            }
+            Divider()
+            HStack {
+                VStack {
+                    Image(systemName: "folder.circle.fill")
+                        .font(.largeTitle)
+                        .padding([.top, .horizontal])
+                        .padding(.bottom, 10)
+                    VStack {
+                        Text("Access to Files")
+                            .bold()
+                            .font(.title2)
+                        Text("When you have a project open, XcodeRPC may request " +
+                             "access to your folders. This is to use your app icons in " +
+                             "Discord and is optional."
+                        )
+                            .frame(width: 300, height: nil, alignment: .leading)
+                            .multilineTextAlignment(.center)
+                    }
+                }
+                VStack {
+                    Image(systemName: "folder.circle.fill")
+                        .font(.largeTitle)
+                        .padding([.top, .horizontal])
+                        .padding(.bottom, 10)
+                    VStack {
+                        Text("Access to Discord")
+                            .bold()
+                            .font(.title2)
+                        Text("XcodeRPC will connect to your Discord Client to set your " +
+                             "activity while you are using Xcode. It can also optionally " +
+                             "save your data locally."
+                        )
+                        .frame(width: 300, height: nil, alignment: .leading)
+                        .multilineTextAlignment(.center)
+                    }
+                }
             }
         }
     }
